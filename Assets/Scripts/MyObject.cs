cead0f8d2dffa17a1cac6f79d3de1dcd1b857518
Emirhan Şenkal <emirhansenkal1@gmail.com>
2021-01-28 16:03:01 +0300

    Initial Project

---
+++ Assets/Scripts/MyObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyObject : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Vector3.forward * (Time.deltaTime * 10));
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private float spawnInterval = 1;
+
+    [SerializeField] private ObjectPool objectPool = null;
+    void Start()
+    {
+        StartCoroutine(nameof(SpawnRoutine));
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        int counter = 0;
+        while (true)
+        {
+
+           GameObject obj = objectPool.GetPooledObject(counter++ % 2);
+           obj.transform.position = Vector3.zero;
+            
+            yield return  new WaitForSeconds(spawnInterval);
+        }
+    }
+
+}
